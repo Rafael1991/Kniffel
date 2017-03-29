63826ab9fb06727695663caccf01dbe0f76928f2
Rafael1991 <n-fabrizius@web.de>
2017-03-29 14:03:41 +0200

    Update Kniffel_Main.java
    added some comments

--- Kniffel_Main.java
+++ Kniffel_Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// die main führt das Spielmenü über while-Schleife aus und über for-Schleife wechselt man zum nächsten Spieler
 public class Kniffel_Main {
 	
 		public static void main (String[] args) {
@@ -9,38 +10,33 @@ public class Kniffel_Main {
 			Scanner sc = new Scanner(System.in);		// Scanner zum Einlesen
 
 
-			 
 			Kniffel spiel = new Kniffel();		// Spiel wird erstellt
 			boolean gameMenue = false;
 			
 			while(gameMenue == false) {
 				
-				spiel.printMenu();
-				int auswahl = sc.nextInt();
+				spiel.printMenu();		//SpielMenü erscheint
+				int auswahl = sc.nextInt();		
 			
 				switch(auswahl) {
-
 
 				case 1:
 
 					Spieler freunde[];
-					freunde = spiel.initializePlayer();
+					freunde = spiel.initializePlayer();		//Spiel startet
 
 					for (int round = 0;round < max_round; round++ ) {
 
 
-						for (int dran = 0; dran < freunde.length ; dran++ ) {
+						for (int dran = 0; dran < freunde.length ; dran++ ) {	//wechselt zum nächsten Spieler
 							System.out.println("\n\nDu bist dran " +freunde[dran].name+ " !\nDein Wurf:");
 							freunde[dran].hand = freunde[dran].zug();
 							freunde[dran].auswertung(freunde[dran].hand);
 						}
 						
 					}
-
-
-					gameMenue = true;
-
-
+					
+					gameMenue = true;		//Ende while-Schleife
 
 				//case 2:
 					
@@ -54,6 +50,7 @@ public class Kniffel_Main {
 										+"\n Spiel beendet");
 					gameMenue = true; // Spiel beenden
 					break;
+					
 				default: 
 					System.out.println("Fehlerhafte Eingabe");
 					break;
